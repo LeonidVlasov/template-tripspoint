fb59b41a8ffe4f6943570cbedf42860c7fd25a10
Leonid Vlasov <leonid.vlasov@gmail.com>
2017-09-05 20:35:32 +0300

    new folder for tripspoint

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var sourcemaps = require('gulp-sourcemaps');
 var rigger = require('gulp-rigger');
- 
+
 var output = './dev/';
 
 gulp.task('html', function () {
@@ -15,19 +15,22 @@ gulp.task('html', function () {
         .pipe(gulp.dest(output));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./dev/assets/sass/**/*.scss')
+gulp.task('sass', () => {
+    return setTimeout(() => {
+    return gulp.src('./dev/assets/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dev/assets/css'));
+    .pipe(gulp.dest('./dev/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+  }, 500)
 });
 
 gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
 			baseDir: output
-		}
+		},
 		browser: ["chrome"]
 	});
 });
@@ -37,6 +40,8 @@ gulp.task('bs-reload', function () {
 });
  
 gulp.task('default', ['html','sass', 'browser-sync'], function () {
-  gulp.watch('./dev/assets/sass/**/*.scss', ['sass', 'bs-reload']);
-  gulp.watch('./dev/templates/**/*.html', ['html', 'bs-reload']);
+  gulp.watch('./dev/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('./dev/templates/**/*.html', ['html']);
 });
+
+    
